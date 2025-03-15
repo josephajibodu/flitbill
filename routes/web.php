2e4f85ccd90f12a58ce16337d31970e78bfe1714
josephajibodu <josephajibodu@gmail.com>
2025-03-15 02:22:28 +0100

    controllers created

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+//    Route::get('buy-data', );
 });
 
 require __DIR__.'/settings.php';
