f0b9932c5676c6ebff8196048296be3a5159d1f4
josephajibodu <josephajibodu@gmail.com>
2025-03-29 19:09:47 +0100

    ide helper installed
    user migrations added

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('other_name')->nullable();
+            $table->string('username');
+            $table->string('phone_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('referral_code');
+            $table->string('gender')->nullable();
+            $table->string('nationality');
+            $table->string('date_of_birth')->nullable();
+            $table->timestamp('banned_at')->nullable();
+            $table->string('banned_reason')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
